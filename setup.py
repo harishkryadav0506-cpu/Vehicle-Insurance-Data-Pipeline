f3219f72c72179d3476b1ca37ce7940e6254786d
Harish Yadav <harishkryadav0506@gmail.com>
2026-06-08 23:47:26 +0530

    project structure ready,venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Harish",
+    author_email="harishkryadav0506@gmail.com",
+    packages=find_packages()
+)
